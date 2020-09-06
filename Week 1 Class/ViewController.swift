d7ea18292f35d22c8ee68022d4e578b0abf480e3
Jennifer Joseph <josephjz@bc.edu>
2020-09-06 15:32:10 -0400

    Added print statement with viewDidLoad() function for us to see in debug pane as the programmer, but not to the user. Also added Date() function.

--- Week 1 Class/ViewController.swift
+++ Week 1 Class/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("🥳viewDidLoad() has executed. NICE!🥳", Date())
+        // Date gives us prime meridian time, we will modify this to be more applicable later in the semester. Month and Day will come up normally, but time will look funky as is. 
     }
 
 

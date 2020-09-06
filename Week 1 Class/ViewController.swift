9ffe6e621e7999396e18668c44817928782cb726
Jennifer Joseph <josephjz@bc.edu>
2020-09-06 15:23:32 -0400

    Initial Commit

---
+++ Week 1 Class/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week 1 Class
+//
+//  Created by Jennifer Joseph on 9/6/20.
+//  Copyright © 2020 Jennifer Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
